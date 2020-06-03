93971e6e899b35e037676aecaca21be49245ba5f
DerpyChap <holla@derpychap.co.uk>
2020-06-03 20:31:30 +0100

    Initial work on custom help formatting

--- cogs/help/help.py
+++ cogs/help/help.py
@@ -5,31 +5,94 @@ from utils import checks
 
 class FancyHelp(commands.HelpCommand):
     COLOUR = discord.Colour.blurple()
+    HIDDEN = "\\🕵️‍"
 
     def get_ending_note(self):
         return 'Use {0}{1} [command] for more info on a command.'.format(self.clean_prefix, self.invoked_with)
 
     def get_command_signature(self, command):
         return '{0.qualified_name} {0.signature}'.format(command)
+    
+    def generate_command_strs(self, commands):
+        max_length = 0
+        is_owner = checks.owner_check(self.context)
+        for command in commands:
+            if not command.hidden or is_owner:
+                length = len(command.name)
+                if length > max_length:
+                    max_length = length
+        command_list = []
+        for command in commands:
+            if not command.hidden or is_owner:
+                string = f'`{command.name}`'
+                if command.hidden:
+                    string += self.HIDDEN
+                for i in range(0, max_length - len(command.name) + 1):
+                    string += '   '
+                string += command.short_doc
+                # Let's set a somewhat reasonable limit
+                if len(string) > 512:
+                    string = string[:509] + '...'
+                command_list.append(string)
+        return command_list
+    
+    def format_commands(self, formatted_commands:dict):
+        embeds = []
+        char_limit = 0
+
+        embed = discord.Embed(colour=self.COLOUR)
+        
+        for field_name, commands in formatted_commands.items():
+            write_desc = False
+            string = ''
+            char_limit += len(field_name)
+            for command in commands:
+                # Grant a little overhead just in case
+                if char_limit + len(command) >= 5500 or len(embed.fields) == 24:
+                    embeds.append(embed)
+                    embed = discord.Embed(colour=self.COLOUR)
+                    char_limit = 8
+                    string = ''
+                    field_name = 'ᅟᅟᅟᅟᅟᅟᅟᅟ'
+                    write_desc = True
+                if len(string) + len(command) + 1 <= (2048 if write_desc else 1024):
+                    to_add = command if not string else f'\n{command}'
+                    char_limit += len(to_add)
+                    string += to_add
+                else:
+                    if write_desc:
+                        embed.description = string
+                    embed.add_field(name=field_name, value=string, inline=False)
+                    string = ''
+                    field_name = 'ᅟᅟᅟᅟᅟᅟᅟᅟ'
+                    char_limit += 8
+            embed.add_field(name=field_name, value=string, inline=False)
+        
+        if embed not in embeds:
+            embeds.append(embed)
+        
+        return embeds
 
     async def send_bot_help(self, mapping):
-        embed = discord.Embed(title='Bot Commands', colour=self.COLOUR)
-        description = self.context.bot.description
-        if description:
-            embed.description = description
-
+        desc_format = {}
         for cog, commands in mapping.items():
             name = 'No Category' if cog is None else cog.qualified_name
             filtered = await self.filter_commands(commands, sort=True)
             if filtered:
-                value = '\u2002'.join(c.name for c in commands)
-                if cog and cog.description:
-                    value = '{0}\n{1}'.format(cog.description, value)
+                formatted = self.generate_command_strs(filtered)
+                desc_format[name] = formatted
+        if desc_format:
+            embeds = self.format_commands(desc_format)
+        else:
+            embeds = [discord.Embed(colour=self.COLOUR)]
+        embeds[0].title = 'Bot Commands'
+        description = self.context.bot.description
+        if description:
+            embeds[0].description = description
 
-                embed.add_field(name=name, value=value)
-
-        embed.set_footer(text=self.get_ending_note())
-        await self.get_destination().send(embed=embed)
+        embeds[-1].set_footer(text=self.get_ending_note())
+        for embed in embeds:
+            await self.get_destination().send(embed=embed)
 
     async def send_cog_help(self, cog):
         embed = discord.Embed(title='{0.qualified_name} Commands'.format(cog), colour=self.COLOUR)
@@ -44,21 +107,25 @@ class FancyHelp(commands.HelpCommand):
         await self.get_destination().send(embed=embed)
 
     async def send_group_help(self, group):
-        embed = discord.Embed(title=group.qualified_name, colour=self.COLOUR)
-        if group.help:
-            embed.description = group.help
-
+        embeds = []
         if isinstance(group, commands.Group):
             filtered = await self.filter_commands(group.commands, sort=True)
-            for command in filtered:
-                embed.add_field(name=self.get_command_signature(command), value=command.short_doc or '...', inline=False)
+            if filtered:
+                formatted = self.generate_command_strs(filtered)
+                embeds = self.format_commands({'Subcommands': formatted})
 
-        embed.set_footer(text=self.get_ending_note())
-        await self.get_destination().send(embed=embed)
+        if not embeds:
+            embeds = [discord.Embed(colour=self.COLOUR)]
+        
+        embeds[0].title = group.qualified_name
 
-    # This makes it so it uses the function above
-    # Less work for us to do since they're both similar.
-    # If you want to make regular command help look different then override it
+        if group.help:
+            embeds[0].description = group.help
+
+        embeds[-1].set_footer(text=self.get_ending_note())
+        for embed in embeds:
+            await self.get_destination().send(embed=embed)
+
     send_command_help = send_group_help
 
 
