8d3851b92b948e13ade3af00a3cd4268092f8b04
DerpyChap <holla@derpychap.co.uk>
2020-06-06 15:27:32 +0100

    Fix some things and improve get_safe function

--- utils/yaml.py
+++ utils/yaml.py
@@ -1,6 +1,9 @@
 import strictyaml
 import os
 import errno
+import logging
+
+logger = logging.getLogger('heleus')
 
 folder = os.environ.get('HELEUS_CONFIG', 'config')
 
@@ -11,10 +14,14 @@ def get(file:str, schema:strictyaml.Map = None):
     if not os.path.exists(path):
         raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), f'{folder}/{file}')
     with open(path, 'r') as f:
-        return strictyaml.load(f, schema)
+        print(f)
+        return strictyaml.load(f.read(), schema).data
 
 def get_safe(file:str, schema:strictyaml.Map = None):
     try:
         return get(file, schema)
-    except:
+    except FileNotFoundError:
+        return None
+    except Exception:
+        logger.exception(f'Failed to parse help file: {file}')
         return None
